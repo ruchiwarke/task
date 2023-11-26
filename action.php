f47d26e395feb71bb7fde71569f3d79552d0ae1e
ruchiwarke <ruchiwarke16@gmail.com>
2023-11-26 16:01:52 +0530

    updated

--- action.php
+++ action.php
@@ -11,5 +11,19 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+// get the post records
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// database insert SQL code
+$sql = "INSERT INTO `task_db` (`id`, `username`, `password`) VALUES ('1', '$username', '$password')";
+
+// insert in database 
+$rs = mysqli_query($conn, $sql);
+
+if($rs)
+{
+	echo "Contact Records Inserted";
+}
+
 ?>
